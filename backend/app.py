af8516d1a0b7a1d6693ffeb3639ea1520b754556
Mahadev Singh <mahadevsingh625@gmail.com>
2025-06-04 13:54:09 +0530

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Allow React to connect
+
+@app.route("/")
+def home():
+    return "Backend is running!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
